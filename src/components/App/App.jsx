faa9907d0c9803fcaf0aa3a44d4446c65fe7e0e4
Anna Parshukova <annpaarshukova041@gmail.com>
2024-08-20 12:36:13 +0300

    add index.css

---
+++ src/components/App/App.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react'
+
+import './App.css'
+import Description from '../Description/Description'
+import Options from '../Options/Options'
+import Feedback from '../Feedback/Feedback'
+import Notification from '../Notification/Notification'
+
+
+
+function App() {
+  const [grades, setGrade] = useState(() => {
+    const savedGrades = JSON.parse(localStorage.getItem("saved-stan")) 
+
+    if (savedGrades !== null) {
+      return savedGrades;
+    }
+    else {
+      return {good: 0, neutral: 0, bad: 0}
+    }
+	
+  })
+
+  useEffect(() => {
+  localStorage.setItem("saved-stan",JSON.stringify(grades))
+   },[grades])
+
+
+  const updateFeedback = (feedbackType) => {
+      // Тут використовуй сеттер, щоб оновити стан
+      setGrade({ ...grades,  [feedbackType]: grades[feedbackType]+1})
+  }
+    
+  const resetState = () => {
+  setGrade({good: 0, neutral: 0,	bad: 0})
+  }
+
+   const totalFeedback = grades.good + grades.bad + grades.neutral;
+   const positiveFeedback = totalFeedback > 0? Math.round((grades.good/ totalFeedback) * 100):0
+
+   
+ 
+  return (
+      <>
+          <Description />
+          <Options listStats={grades} handleClick={updateFeedback} totalFeedback={totalFeedback} resetState={resetState} />
+          { totalFeedback === 0 ? <Notification>No feedback yet</Notification> : <Feedback listStats={grades} totalFeedback={totalFeedback} positiveFeedback={positiveFeedback}  />}
+          
+      </>
+  )
+}
+
+
+export default App
